d8d405a30563c4575cc2a00f42036c678b5333ad
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2024-12-14 00:55:19 -0300

    changes

---
+++ Json test/Datos/conexion_db.py
@@ -0,0 +1,61 @@
+import mysql.connector
+from mysql.connector import errorcode
+from Auxiliares.constantes import host, user, password, database
+
+def generar_conexion():
+    config={
+        'user': user,
+        'password': password,
+        'host': host,
+        'database': database
+    }
+    try:
+        conexion = mysql.connector.connect(**config)
+        if conexion and conexion.is_connected():
+            return conexion
+        else:
+            print("No fue posible conectarse a la base de datos.")
+    
+    except mysql.connector.Error as error:
+        if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Acceso denegado, usuario o contraseña incorrectos.")
+        elif error.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Su base de datos NO existe")
+        else:
+            print(error)
+    else:
+        conexion.close()
+        
+def leer_datos(consulta):      
+    conexion = generar_conexion()
+    cursor = conexion.cursor()
+    if cursor != None:
+        cursor.execute(consulta)
+        resultado = cursor.fetchall()
+        cursor.close()    
+        return resultado
+    else:
+        print("Su búsqueda no arrojó resultados...")
+    conexion.close()
+        
+def insertar_datos(consulta):      
+    conexion = generar_conexion()
+    cursor = conexion.cursor()
+    if cursor != None:
+        cursor.execute(consulta)
+        conexion.commit()
+        id = cursor.lastrowid
+        cursor.close()        
+        return print(f"Id registro insertado = {id}")
+    else:
+        print("Su búsqueda no arrojó resultados...")
+    conexion.close()
+
+# from sqlalchemy import create_engine
+# from sqlalchemy import text
+
+# engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
+
+# def  generar_conexion():
+#     conexion = engine.connect()
+#     return conexion
--- Json test/main.py
+++ Json test/main.py
@@ -1,18 +1,115 @@
-from Datos import data_empleados
-import Negocio.encriptacion
-from Negocio.negocio_empleados import listado_empleados as listado
-import Negocio.negocio_usuarios
-import Negocio.generacion_clave
+import Servicios.ConsumirAPI, Datos.conexion_db as DAL.db_conection
 
-# data = listado()
-# print(data)
+# Menú de jsonplaceholder
+def menu_json():
+    """Menú interactivo para utilizar las funciones en jsonplaceholder"""
+    while True:
+        print(""" 
+Menú de JsonPlaceholder:
+1. Consultar Datos de Usuario
+2. Consultar Post
+3. Consultar Tarea
+0. Salir de este menú
+""")
+        try: 
+            option = int(input("Ingrese una opción: "))
+        except ValueError:
+            print("Error: Debe ingresar un número")
+            continue  # Volver al inicio del bucle
 
-Negocio.generacion_clave.generacion_clave()
-# contrasena = input("Ingrese Contraseña:")
-# contrasena_encriptada = ''
-# contrasena_encriptada = negocio.encriptacion.encriptar_contrasena(contrasena)
+        # User
+        if option == 1:
+            user = Servicios.ConsumirAPI.view_user()
+            if user:
+                answer = input("\n¿Desea Guardar el Usuario en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    DAL.db_conection.save_user_DB(user.userId, user.name, user.username, user.email, user.phone)
+                elif answer == 'no':
+                    print("\nEl Usuario no se ha guardado en la DB\n")
+                    continue
 
-# contrasena = input("Ingrese Contraseña:")
-# contrasena_anterior = negocio.encriptacion.desencriptar_contrasena(contrasena_encriptada)
-# if contrasena_anterior == contrasena:
-#     pass
+        # Post
+        elif option == 2:
+            post = Servicios.ConsumirAPI.read_post()
+            if post:
+                answer = input("\n¿Desea Guardar el post en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    DAL.db_conection.save_post_DB(post.id, post.title, post.body)
+                elif answer == 'no':
+                    print("\nEl Post no se ha guardado en la DB\n")
+                    continue
+
+        # Tarea
+        elif option == 3:
+            todo = Servicios.ConsumirAPI.view_todos()
+            if todo:
+                answer = input("\n¿Desea Guardar la Tarea en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    DAL.db_conection.save_todo(todo.Id, todo.title, todo.completed)
+                elif answer == 'no':
+                    print("\nLa Tarea no se ha guardado en la DB\n")
+
+        #Salir
+        elif option == 0:
+            print("Volviendo al menú principal...")
+            break
+
+def menu_db():
+    """Menú interacctivo para utilizar las funciones de la DB"""
+    while True:
+        print("""
+Menú de la Base de Datos:
+1. Mostrar Usuarios
+2. Mostrar Posts
+3. Mostrar Tareas
+4. Asignar Post a un Usuario
+5. Asignar Tarea a un Usuario
+0. Salir de este menú
+""")
+        try:
+            option = int(input("Ingrese una opción: "))
+        except ValueError:
+            print("Error: Debe ingresar un número")
+            return
+
+        # Ver Users
+        if option == 1:
+            DAL.db_conection.view_user_DB()
+            continue
+        # Ver Posts
+        if option == 2:
+            DAL.db_conection.view_post_DB()
+            continue
+        # Ver Tareas
+        if option == 3:
+            DAL.db_conection.view_todo_DB()
+            continue
+        # Asignar Post
+        if option == 4:
+            DAL.db_conection.asign_post()
+            continue
+        # Asignar Tarea
+        if option == 5:
+            DAL.db_conection.asign_todo()
+            continue
+        # Salir
+        if option == 0:
+            print("Volviendo al Menú principal...")
+            break
+
+# Ejecutar el programa
+if __name__ == "__main__":
+    while True:
+        print("""
+Menú del Programa:
+1. Menú de JsonPlaceholder.
+2. Menú de la Base de Datos. 
+0. Salir""")
+        option = int(input("Ingrese una opción: "))
+        if option == 1:
+            menu_json()
+        elif option == 2:
+            menu_db()
+        elif option == 0:
+            print("Saliendo del programa...")
+            break
