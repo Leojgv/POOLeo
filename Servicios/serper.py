3c694e189c51b013f5c86fbf4afabab41247fdd9
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2024-12-15 16:04:13 -0300

    Cambios carpeta

---
+++ Servicios/serper.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import Auxiliares.constantes
+
+url = Auxiliares.constantes.URL_SERPER
+def menu_serper():
+    # Solicitar al usuario que ingrese la query
+    query = input("Ingrese la query de búsqueda: ")
+
+    payload = json.dumps({
+        "q": query  # Usar la query ingresada por el usuario
+    })
+    headers = {
+        'X-API-KEY': Auxiliares.constantes.APIKEY, # Esto tiene que cambiarlo el profe.
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(Auxiliares.constantes.URL_SERPER, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        results = response.json()
+        print("Resultados de la búsqueda:")
+        for result in results.get('organic', []):
+            print(f"Título: {result['title']}")
+            print(f"URL: {result['link']}")
+            print(f"Descripción: {result['snippet']}\n")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
---
+++ main.py
@@ -0,0 +1,127 @@
+import Servicios.InteractAPI, Datos.conexion_db, Servicios.Managment_Contrasena
+import Datos.conexion_db
+import Servicios.Servicio_serper
+import Servicios.serper
+
+# Menú de jsonplaceholder
+def menu_json():
+    """Menú interactivo para utilizar las funciones en jsonplaceholder"""
+    while True:
+        print(""" 
+Menú JsonPlaceholder:
+1. Consultar Datos de Usuario
+2. Consultar Post
+3. Consultar Tarea
+0. Salir de este menú
+""")
+        try: 
+            option = int(input("Ingrese una opción: "))
+        except ValueError:
+            print("Error: Debe ingresar un número")
+            continue  # Volver al inicio del bucle
+
+        # User
+        if option == 1:
+            user = Servicios.InteractAPI.view_user()
+            if user:
+                answer = input("\n¿Desea Guardar el Usuario en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    Datos.conexion_db.save_user_DB(user.userId, user.name, user.username, user.email, user.phone)
+                elif answer == 'no':
+                    print("\nEl Usuario no se ha guardado en la DB\n")
+                    continue
+
+        # Post
+        elif option == 2:
+            post = Servicios.InteractAPI.read_post()
+            if post:
+                answer = input("\n¿Desea Guardar el post en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    Datos.conexion_db.save_post_DB(post.id, post.title, post.body)
+                elif answer == 'no':
+                    print("\nEl Post no se ha guardado en la DB\n")
+                    continue
+
+        # Tarea
+        elif option == 3:
+            todo = Servicios.InteractAPI.view_todos()
+            if todo:
+                answer = input("\n¿Desea Guardar la Tarea en la Base de Datos? (si/no): ")
+                if answer == 'si':
+                    Datos.conexion_db.save_todo(todo.Id, todo.title, todo.completed)
+                elif answer == 'no':
+                    print("\nLa Tarea no se ha guardado en la DB\n")
+
+        #Salir
+        elif option == 0:
+            print("Volviendo al menú principal...")
+            break
+
+def menu_db():
+    """Menú interacctivo para utilizar las funciones de la DB"""
+    while True:
+        print("""
+Menú Base de Datos:
+1. Mostrar Usuarios
+2. Mostrar Posts
+3. Mostrar Tareas
+4. Asignar Post a un Usuario
+5. Asignar Tarea a un Usuario
+6. Desencriptar Contraseña
+0. Salir de este menú
+""")
+        try:
+            option = int(input("Ingrese una opción: "))
+        except ValueError:
+            print("Error: Debe ingresar un número")
+            return
+
+        # Ver Users
+        if option == 1:
+            Datos.conexion_db.view_user_DB()
+            continue
+        # Ver Posts
+        elif option == 2:
+            Datos.conexion_db.view_post_DB()
+            continue
+        # Ver Tareas
+        elif option == 3:
+            Datos.conexion_db.view_todo_DB()
+            continue
+        # Asignar Post
+        elif option == 4:
+            Datos.conexion_db.asign_post()
+            continue
+        # Asignar Tarea
+        elif option == 5:
+            Datos.conexion_db.asign_todo()
+            continue
+        elif option == 6:
+            user_id = input("Ingrese el ID del usuario para desencriptar: ")
+            Datos.conexion_db.decrypt_password_from_db(user_id)
+        elif option == 0:
+            print("Volviendo al Menú principal...")
+            break
+
+#Menu Principal 
+if __name__ == "__main__":
+    while True:
+        print("""
+Menú Principal:
+1. Menú JsonPlaceholder.
+2. Menú Base de Datos.
+3. Menú Encriptación.
+4. Menú Serper.
+0. Salir""")
+        option = int(input("Ingrese una opción: "))
+        if option == 1:
+            menu_json()
+        elif option == 2:
+            menu_db()
+        elif option == 3:
+            Servicios.Managment_Contrasena.password_management()
+        elif option == 4:
+            Servicios.serper.menu_serper()
+        elif option == 0:
+            print("Saliendo del programa...")
+            break
